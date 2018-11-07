1f3b2e7578aeaa213d9194e9adef6aa9c7d43347
Ambuj Bhog <ambujbhog@protonmail.com>
2018-11-07 02:12:49 -0500

    Introduced the first event listeners
    Added listeners for a 'mouse click' and 'enter key' on the add__button which will update the UI.

--- app.js
+++ app.js
@@ -20,8 +20,30 @@ var UIController = (function(){
 //Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        console.log('button was clicked')
+    var ctrlAddItem = function(){
+
+        // 1. Get the input data from the field
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the User Interface
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+        console.log('It works');
+
+    }
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event){
+        // Check if 'enter' is pressed and do the same thing as input button above
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+        }
+        
     });
 
 })(budgetController, UIController);
+
