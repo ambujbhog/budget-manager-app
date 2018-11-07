4c8f923100da8a9980bae55a43d7c37e42eb23bd
Ambuj Bhog <ambujbhog@protonmail.com>
2018-11-07 01:45:32 -0500

    initial-commit
    I just setted up the project. Nothing interesting here, but the base of the project.

---
+++ app.js
@@ -0,0 +1,25 @@
+
+//Budget Controller
+var budgetController = (function(){
+    
+
+
+
+})();
+
+
+// UI Controller
+var UIController = (function(){
+    //Some Code
+
+
+
+})();
+
+
+//Global App Controller
+var controller = (function(budgetCtrl, UICtrl){
+
+    
+
+})(budgetController, UIController);
