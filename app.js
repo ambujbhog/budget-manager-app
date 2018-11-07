d93b840d835ed9505ada3014999c2ba5ecc1ef16
Ambuj Bhog <ambujbhog@protonmail.com>
2018-11-07 02:45:00 -0500

    Added the functionality to read input data

--- app.js
+++ app.js
@@ -10,9 +10,28 @@ var budgetController = (function(){
 
 // UI Controller
 var UIController = (function(){
-    //Some Code
 
+    var DOMstrings = { // All strings are stored here 
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
+    }
 
+    return{
+        getinput: function(){
+
+            return {
+             type: document.querySelector(DOMstrings.inputType).value,
+             description: document.querySelector(DOMstrings.inputDescription).value,
+             value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+        getDOMstrings: function(){ //Making private DOMstrings public
+            return DOMstrings;
+
+        }
+    };
 
 })();
 
@@ -20,9 +39,13 @@ var UIController = (function(){
 //Global App Controller
 var controller = (function(budgetCtrl, UICtrl){
 
+    var DOM = UICtrl.getDOMstrings(); // Getting DOMstrings
+
     var ctrlAddItem = function(){
 
         // 1. Get the input data from the field
+        var input = UICtrl.getinput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -31,11 +54,10 @@ var controller = (function(budgetCtrl, UICtrl){
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-        console.log('It works');
-
+        
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event){
         // Check if 'enter' is pressed and do the same thing as input button above
