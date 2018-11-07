5fec7b754cfcc21509d4d988de2f9593a01238ad
Ambuj Bhog <ambujbhog@protonmail.com>
2018-11-07 03:20:34 -0500

    Added Income and Expense Constructors

--- app.js
+++ app.js
@@ -2,7 +2,29 @@
 //Budget Controller
 var budgetController = (function(){
     
+var Expense = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
 
+var Income = function(id, description, value){
+    this.id = id;
+    this.description = description;
+    this.value = value;
+};
+
+var data = {
+    allItem: {
+        exp: [],
+        inc: []
+    },
+    totals: {
+        exp: 0,
+        inc: 0
+    }
+    
+};
 
 
 })();
